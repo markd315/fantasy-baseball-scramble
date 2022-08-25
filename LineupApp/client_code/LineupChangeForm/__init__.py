c8ae92ccdd4b4edcfaa88f2077cdff93d98f4958
markd315 <markd315@gmail.com>
2022-08-25 00:14:57 -0400

    progress

--- LineupApp/client_code/LineupChangeForm/__init__.py
+++ LineupApp/client_code/LineupChangeForm/__init__.py
@@ -5,11 +5,18 @@ import anvil.server
 class LineupChangeForm(LineupChangeTemplate):
 
   def __init__(self, **properties):
-    self.init_components(**properties)
+    self.init_components_lineup(**properties)
     self.set_lineup.set_event_handler('click', self.set_lineup_click)
     #self.lineup_json.set_event_handler('pressed_enter', self.set_lineup_click)  # only works for textBox I guess?
     self.set_lineup.set_event_handler('click', self.set_lineup_click)
     self.get_lineup.set_event_handler('click', self.get_lineup_click)
+    self.get_roster.set_event_handler('click', self.get_roster_click)
+    self.add_lineup.set_event_handler('click', self.add_lineup_click)
+    self.drop_lineup.set_event_handler('click', self.drop_lineup_click)
+
+    self.link_lineup.set_event_handler('click', self.init_components_lineup)
+    self.link_add.set_event_handler('click', self.init_components_add)
+    self.link_drop.set_event_handler('click', self.init_components_drop)
 
   
   def get_lineup_click(self, **event_args):
@@ -21,7 +28,14 @@ class LineupChangeForm(LineupChangeTemplate):
     if self.league_name.text and self.team_name.text:
       anvil.server.call('set_lineup', self.league_name.text, self.team_name.text, self.lineup_json.text)
 
+  def drop_lineup_click(self, **event_args):
+    json = anvil.server.call('get_lineup', self.league_name.text, self.team_name.text)
+    self.lineup_json.text = json
 
+  def add_lineup_click(self, **event_args):
+    json = anvil.server.call('get_lineup', self.league_name.text, self.team_name.text)
+    self.lineup_json.text = json
 
-
-
+  def get_roster_click(self, **event_args):
+    list = anvil.server.call('get_lineup', self.league_name.text, self.team_name.text)
+    self.roster.text = list
--- LineupApp/client_code/LineupChangeForm/_template.py
+++ LineupApp/client_code/LineupChangeForm/_template.py
@@ -2,27 +2,78 @@ from anvil import *
 
 class LineupChangeTemplate(HtmlPanel):
 
-  def init_components(self, **properties):
+  def init_components_base(self, **properties):
     super().__init__()
+    self.clear()
     self.html = '@theme:standard-page.html'
     self.content_panel = GridPanel()
     self.add_component(self.content_panel)
     self.title_label = Label(text="Fantasy Lineup Setter")
     self.add_component(self.title_label, slot="title")
     self.card_1 = GridPanel(role="card")
-    self.set_lineup_panel = FlowPanel(align="center")
-    self.card_1.add_component(self.set_lineup_panel,
-                              row="A", col_sm=2, width_sm=10)
+    self.main_content = FlowPanel(align="center")
+    self.col = ColumnPanel()
+    self.sidebar = FlowPanel(align="left")
+    self.sidebar.add_component(self.col)
+    self.link_lineup = Link(text="Set Lineup")
+    self.link_add = Link(text="Add to roster")
+    self.link_drop = Link(text="Drop from roster")
+    self.col.add_component(self.link_lineup, slot="sidebar")
+    self.col.add_component(self.link_add, slot="sidebar")
+    self.col.add_component(self.link_drop, slot="sidebar")
+    self.add_component(self.sidebar)
 
-    # Unique code for this SPA starts here
-    self.league_name = TextBox(placeholder="League Name", width=400)
+
+  def init_components_lineup(self, **properties):
+    self.init_components_base()
+    self.league_name = TextBox(placeholder="League Name", width=400, text="livetestleague")
     self.team_name = TextBox(placeholder="Team Code", width=400)
     self.lineup_json = TextArea(placeholder="Team Lineup", width=500, height=700)
     self.get_lineup = Button(text="Get Current Lineup", role="primary-color")
     self.set_lineup = Button(text="Set Lineup", role="secondary-color")
-    self.set_lineup_panel.add_component(self.league_name)
-    self.set_lineup_panel.add_component(self.team_name)
-    self.set_lineup_panel.add_component(self.lineup_json)
-    self.set_lineup_panel.add_component(self.get_lineup)
-    self.set_lineup_panel.add_component(self.set_lineup)
+    self.main_content.add_component(self.league_name)
+    self.main_content.add_component(self.team_name)
+    self.main_content.add_component(self.lineup_json)
+    self.main_content.add_component(self.get_lineup)
+    self.main_content.add_component(self.set_lineup)
+    self.card_1.add_component(self.main_content,
+                              row="A", col_sm=2, width_sm=10)
+    self.content_panel.add_component(self.card_1, row="D", col_sm=2, width_sm=8)
+
+  def init_components_add(self, **properties):
+    self.init_components_base()
+    self.league_name = TextBox(placeholder="League Name", width=400, text="livetestleague")
+    self.team_name = TextBox(placeholder="Team Code", width=400)
+    self.player_name = TextBox(placeholder="Player Name", width=400)
+    self.roster = TextArea(placeholder="Roster", width=500,
+                                height=700)
+    self.get_roster = Button(text="Get Current Roster", role="primary-color")
+    self.add_lineup = Button(text="Add Player", role="secondary-color")
+    self.main_content.add_component(self.league_name)
+    self.main_content.add_component(self.team_name)
+    self.main_content.add_component(self.player_name)
+    self.main_content.add_component(self.roster)
+    self.main_content.add_component(self.get_roster)
+    self.main_content.add_component(self.add_lineup)
+    self.card_1.add_component(self.main_content,
+                              row="A", col_sm=2, width_sm=10)
+    self.content_panel.add_component(self.card_1, row="D", col_sm=2, width_sm=8)
+
+  def init_components_drop(self, **properties):
+    self.init_components_base()
+    self.league_name = TextBox(placeholder="League Name", width=400, text="livetestleague")
+    self.team_name = TextBox(placeholder="Team Code", width=400)
+    self.player_name = TextBox(placeholder="Player Name", width=400)
+    self.roster = TextArea(placeholder="Roster", width=500,
+                                height=700)
+    self.get_roster = Button(text="Get Current Roster", role="primary-color")
+    self.drop_lineup = Button(text="Drop Player", role="secondary-color")
+    self.main_content.add_component(self.league_name)
+    self.main_content.add_component(self.team_name)
+    self.main_content.add_component(self.player_name)
+    self.main_content.add_component(self.roster)
+    self.main_content.add_component(self.get_roster)
+    self.main_content.add_component(self.drop_lineup)
+    self.card_1.add_component(self.main_content,
+                              row="A", col_sm=2, width_sm=10)
     self.content_panel.add_component(self.card_1, row="D", col_sm=2, width_sm=8)
