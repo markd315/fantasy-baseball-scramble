c1c3525dd867509bf9bd025992f3a7b2865e9851
markd315 <markd315@gmail.com>
2022-08-25 11:48:49 -0400

    hopefully improve navigation and mobile capability

--- LineupApp/client_code/LineupChangeForm/_template.py
+++ LineupApp/client_code/LineupChangeForm/_template.py
@@ -10,75 +10,85 @@ class LineupChangeTemplate(HtmlPanel):
     self.add_component(self.content_panel)
     self.title_label = Label(text="Fantasy Lineup Setter")
     self.add_component(self.title_label, slot="title")
+
+    # Sidebar
+    self.navbar = FlowPanel(align="right")
+    self.link_lineup = Link(text="Set Lineup")
+    self.link_add = Link(text="Add to roster")
+    self.link_drop = Link(text="Drop from roster")
+    self.navbar.add_component(self.link_lineup, slot="nav-right")
+    self.navbar.add_component(self.link_add, slot="nav-right")
+    self.navbar.add_component(self.link_drop, slot="nav-right")
+    self.add_component(self.navbar, slot='nav-right')
+
     self.card_1 = GridPanel(role="card")
     self.main_content = FlowPanel(align="center")
     self.card_1.add_component(self.main_content,
                               row="A", col_sm=2, width_sm=10)
     self.content_panel.add_component(self.card_1, row="D", col_sm=2, width_sm=8)
 
-    # Sidebar
-    self.link_lineup = Link(text="Set Lineup")
-    self.link_add = Link(text="Add to roster")
-    self.link_drop = Link(text="Drop from roster")
-
     # Parts
-    self.league_name = TextBox(placeholder="League Name", width=400,
-                               text="livetestleague")
-    self.team_name = TextBox(placeholder="Team Code", width=400)
-    self.lineup_json = TextArea(placeholder="Team Lineup", width=500,
-                                height=700)
+    self.league_name = TextBox(placeholder="League Name", text="livetestleague")
+    self.team_name = TextBox(placeholder="Team Code")
+    self.lineup_json = TextArea(placeholder="Team Lineup",height=700)
     self.get_lineup = Button(text="Get Current Lineup", role="primary-color")
     self.set_lineup = Button(text="Set Lineup", role="secondary-color")
     self.get_roster = Button(text="Get Current Roster", role="primary-color")
     self.drop_lineup = Button(text="Drop Player", role="secondary-color")
     self.add_lineup = Button(text="Add Player", role="secondary-color")
-    self.roster = TextArea(placeholder="Roster", width=500,
-                           height=700)
-    self.player_name = TextBox(placeholder="Player Name", width=400)
+    self.roster = TextArea(placeholder="Roster", height=700)
+    self.player_name = TextBox(placeholder="Player Name")
+    self.addComponent(self.league_name)
+    self.addComponent(self.team_name)
+    self.addComponent(self.lineup_json)
+    self.addComponent(self.get_lineup)
+    self.addComponent(self.set_lineup)
+    self.addComponent(self.roster)
+    self.addComponent(self.get_roster)
+    self.addComponent(self.add_lineup)
+    self.addComponent(self.drop_lineup)
+    self.addComponent(self.player_name)
 
-    # Rest of sidebar
-    self.col = ColumnPanel()
-    self.sidebar = FlowPanel(align="left")
-    self.sidebar.add_component(self.col)
-    self.col.add_component(self.link_lineup, slot="sidebar")
-    self.col.add_component(self.link_add, slot="sidebar")
-    self.col.add_component(self.link_drop, slot="sidebar")
-    self.add_component(self.sidebar)
 
   def addComponent(self, component):
     try:
       component.remove_from_parent()
     except BaseException:
       pass
+    component.visible = True
     self.main_content.add_component(component)
 
 
   def show_lineup_page(self, **properties):
-    self.addComponent(self.league_name)
-    self.addComponent(self.team_name)
-    self.addComponent(self.lineup_json)
-    self.addComponent(self.get_lineup)
-    self.addComponent(self.set_lineup)
-    #self.content_panel.add_component(self.card_1, row="D", col_sm=2, width_sm=8)
+    self.lineup_json.visible = True
+    self.get_lineup.visible = True
+    self.set_lineup.visible = True
+    self.roster.visible = False
+    self.get_roster.visible = False
+    self.add_lineup.visible = False
+    self.drop_lineup.visible = False
+    self.player_name.visible = False
 
 
   def show_add_page(self, **properties):
-    self.addComponent(self.league_name)
-    self.addComponent(self.team_name)
-    self.addComponent(self.player_name)
-    self.addComponent(self.roster)
-    self.addComponent(self.get_roster)
-    self.addComponent(self.add_lineup)
-    #self.content_panel.add_component(self.card_1, row="D", col_sm=2, width_sm=8)
+    self.lineup_json.visible = False
+    self.get_lineup.visible = False
+    self.set_lineup.visible = False
+    self.roster.visible = True
+    self.get_roster.visible = True
+    self.add_lineup.visible = True
+    self.drop_lineup.visible = False
+    self.player_name.visible = True
 
   def show_drop_page(self, **properties):
-    self.addComponent(self.league_name)
-    self.addComponent(self.team_name)
-    self.addComponent(self.player_name)
-    self.addComponent(self.roster)
-    self.addComponent(self.get_roster)
-    self.addComponent(self.drop_lineup)
-    #self.content_panel.add_component(self.card_1, row="D", col_sm=2, width_sm=8)
+    self.lineup_json.visible = False
+    self.get_lineup.visible = False
+    self.set_lineup.visible = False
+    self.roster.visible = True
+    self.get_roster.visible = True
+    self.add_lineup.visible = False
+    self.drop_lineup.visible = True
+    self.player_name.visible = True
 
   def addHandlers(self):
     if hasattr(self, "set_lineup"):
